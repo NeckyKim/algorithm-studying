d82171ecede6c8b702e42b16e26853143cfb2ea1
NeckyKim <kyeryoong@gmail.com>
2023-01-13 03:46:33 +0900

    studied through Q11. (230113)

--- CH4. Implementation/Q11.py
+++ CH4. Implementation/Q11.py
@@ -1,81 +1,85 @@
 # Q11. 뱀
-# [백준] www.acmicpc.net/problem/3190
-
-import time
-import os
+# [백준] https://www.acmicpc.net/problem/3190
 
 
+# 뱀이 이동할 지도
 field_size = int(input())
-
 fields = [[0] * field_size for _ in range(0, field_size)]
 
+# 사과 위치
 apples = [list(map(int, input().split())) for _ in range(0, int(input()))]
 apples = [[apple[0] - 1, apple[1] - 1] for apple in apples]
 
+# 회전 정보
 moves = [list(input().split()) for _ in range(0, int(input()))]
 moves = [[int(move[0]), move[1]] for move in moves]
 
+# 뱀의 길이
 snakes = [[0, 0]]
 
+
+# 지도에 사과를 표시
 for apple in apples:
     fields[apple[0]][apple[1]] = 1
 
-
-def printField():
-    os.system("cls")
-
-    for i in range(0, field_size):
-        for j in range(0, field_size):
-            if [i, j] in snakes:
-                print("X", end=" ")
-            else:
-                print(fields[i][j], end=" ")
-        print("")
-
-    time.sleep(0.5)
-
-
+# 좌표 초기화
 y = 0
 x = 0
 
 
+# 회전 방향
 dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
 
+d = 0
 
-direction = 0
-
+# 시간 계산
 count = 0
 
-for move in moves:
-    for _ in range(0, move[0]):
-        printField()
-        
-        next_y = y + dy[direction]
-        next_x = x + dx[direction]
-        
-        if next_x >= field_size or next_y >= field_size:
-            print(count)
-            exit(0)
+i = 0
 
-        snakes.append([next_y, next_x])
 
-        if [next_y, next_x] in apples:
-            fields[next_y][next_x] = 0
-        
+while True:
+    count = count + 1
+
+    # 뱀이 앞으로 이동할 칸 계산
+    ny = y + dy[d]
+    nx = x + dx[d]
+
+    # 회전하는 시간인지 확인
+    if i < len(moves):
+        if count == moves[i][0]:
+            if moves[i][1] == "D":
+                d = (d + 1) % 4
+
+            else:
+                d = (d - 1) % 4
+
+            i = i + 1
+
+    # 뱀이 지도 안에 있는지 확인
+    if 0 <= nx < field_size and 0 <= ny < field_size:
+
+        # 뱀의 머리가 꼬리에 부딪치면
+        if [ny, nx] in snakes:
+            break
+
         else:
-            snakes.pop(0)
-            
-        count = count + 1
+            snakes.append([ny, nx])
 
-        y = next_y
-        x = next_x
-        
+            # 뱀 앞에 사과가 있는 경우
+            if fields[ny][nx] == 1:
+                fields[ny][nx] = 0
 
-    if move[1] == "D":
-        direction = (direction + 1) % 4
-        count = count + 1
+            # 뱀 앞에 사과가 없는 경우
+            elif fields[ny][nx] == 0:
+                snakes.pop(0)
 
-    if move[1] == "L":
-        direction = (direction - 1) % 4
-        count = count + 1
+            y = ny
+            x = nx
+
+    else:
+        break
+
+
+print(count)
